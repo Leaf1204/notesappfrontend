1eb1d0e86f4eae6331633df967b3b5bc777403be
Leanne A Frisinger <67919464+Leaf1204@users.noreply.github.com>
2020-12-09 15:35:57 -0700

    spelling error fix

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./styles.css";
 import {App} from "./components/App"
-import {BrowsterRouter as Router} from "react-router-dom"
-import {AppState} from "./AppState"
+import {BrowserRouter as Router} from "react-router-dom"
+import {AppState} from "./AppState.jsx"
 
+console.log(Router, App)
 ReactDom.render(<AppState><Router><App /></Router></AppState>, document.querySelector("#root"));
